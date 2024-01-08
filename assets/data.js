8e411140e22332f88e70a3413745999469dcfb03
Naahiki <112497828+Naahiki@users.noreply.github.com>
2024-01-08 19:15:57 +0100

    score v2-results?

--- assets/data.js
+++ assets/data.js
@@ -26,52 +26,9 @@ function addToUserImage(key, value) {
     // Mostrar los datos que se enviarán por la API
     console.log("Datos a enviar por la API:", imagesAsBase64);
 
-    // Llamar a la función para enviar los datos por la API
-    enviarDatosPorAPI(imagesAsBase64);
 
     return imagesAsBase64;
   }
 
   return null; // Si aún no se han agregado las tres imágenes
-}
-
-function enviarDatosPorAPI() {
-  const imageKeys = Object.keys(userImage);
-  if (imageKeys.length === 3) {
-    const formData = new FormData();
-
-    // Agregar cada imagen a FormData (simulando que se envían en base64)
-    imageKeys.forEach(imageKey => {
-      formData.append(imageKey, userImage[imageKey]);
-    });
-
-    // Mostrar los datos que se enviarán en la consola
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ', ' + pair[1]);
-    }
-
-    // Realizar la solicitud POST a JSONPlaceholder (endpoind de ejemplo)
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      // Manejar la respuesta de la API
-      if (response.ok) {
-        return response.json(); // Si la respuesta es JSON
-      } else {
-        throw new Error('Error al enviar los datos');
-      }
-    })
-    .then(data => {
-      // Manejar la respuesta de la API, si devuelve datos
-      console.log('Respuesta de la API:', data);
-    })
-    .catch(error => {
-      // Manejar cualquier error durante la solicitud
-      console.error('Error:', error);
-    });
-  } else {
-    console.error('No se han agregado las tres imágenes');
-  }
 }
